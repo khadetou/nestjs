ee86c6ca19ff5e390dbcf196650780d7b65a1dfc
khadetou <khadetou96@gmail.com>
2021-11-22 18:23:58 +0000

    database relation

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,11 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Task } from 'src/tasks/task.entity';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-    @Column({unique: true})
-    username: string;
-    @Column()
-    password: string;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ unique: true })
+  username: string;
+  @Column()
+  password: string;
+
+  @OneToMany((type) => Task, (tasks) => tasks.user, { eager: true })
+  tasks: Task[];
 }
--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,15 +1,25 @@
-import { TaskStatus } from "./task-status-enum";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { TaskStatus } from './task-status-enum';
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Entity()
 export class Task {
-    //If we didn't use uuid we would have postgresql specific id starting from 1 and so on and it would be of type number
-    @PrimaryGeneratedColumn("uuid")
-    id: string;
-    @Column()
-    title: string;
-    @Column()
-    description: string;
-    @Column()
-    status: TaskStatus;
+  //If we didn't use uuid we would have postgresql specific id starting from 1 and so on and it would be of type number
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  title: string;
+  @Column()
+  description: string;
+  @Column()
+  status: TaskStatus;
+
+  @ManyToOne((type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
