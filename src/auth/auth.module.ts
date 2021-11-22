ea8b8764a914f0a91511b0345541b64e706139ea
khadetou <khadetou96@gmail.com>
2021-11-22 17:29:06 +0000

    jwt passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.respository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     imports:[
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: 'topSecret51',
+            signOptions: {
+                expiresIn: 3600,
+            }
+        }),
        TypeOrmModule.forFeature([UserRepository]),
     ],
   providers: [AuthService],
