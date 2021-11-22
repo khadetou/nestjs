43f2a1341c530a483928f901ea1aa49ac36d98f2
khadetou <khadetou96@gmail.com>
2021-11-22 15:01:54 +0000

    password regex

--- src/auth/dto/auth-crendentials.dto.ts
+++ src/auth/dto/auth-crendentials.dto.ts
@@ -1,5 +1,18 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    {
+        message: 'Password too weak'
+    })
     password: string;
+
 }
