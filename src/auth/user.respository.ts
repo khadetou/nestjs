da4a7a22341ff7e9b3f4d9722f44841b77b933fd
khadetou <khadetou96@gmail.com>
2021-11-22 17:05:40 +0000

    password hashing with Bcryp

--- src/auth/user.respository.ts
+++ src/auth/user.respository.ts
@@ -2,13 +2,18 @@ import { AuthCredentialsDto } from './dto/auth-crendentials.dto';
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
-
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
-        const user = this.create({ username, password });
+
+        //hash the password
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = this.create({ username, password:hashedPassword });
         try {
             await this.save(user);
         } catch (error) {
